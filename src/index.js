9532c94c0d0610517c4549cb100e1b9fa7309528
sidwebworks <xodedeveloper@gmail.com>
2021-10-06 11:48:12 +0530

    .

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
 
-const DATA = fs.readFileSync(path.join(__dirname, "./data.json"), "utf-8");
+const DATA = fs.readFileSync(path.join(process.cwd(), "/src/data.json"), "utf-8");
 
 const App = express();
 
@@ -16,6 +16,7 @@ App.use(cors());
 
 App.get("/", (req, res, next) => {
   const quotes = JSON.parse(DATA);
+
   return res.status(200).json(quotes);
 });
 
