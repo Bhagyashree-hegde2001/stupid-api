cc2c0e440ea3e83ed8bc01f68c4892bf692cc12b
sidwebworks <xodedeveloper@gmail.com>
2021-10-06 11:41:43 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const helmet = require("helmet");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+
+const DATA = fs.readFileSync(path.join(__dirname, "./data.json"), "utf-8");
+console.log("DATA: ", DATA);
+
+const App = express();
+
+App.use(express.json());
+
+App.use(helmet());
+
+App.use(cors());
+
+App.get("/", (req, res, next) => {
+  const quotes = JSON.parse(DATA);
+  return res.status(200).json(quotes);
+});
+
+App.listen(3000);
